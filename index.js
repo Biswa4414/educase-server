cc7dc2f180952303acb848d53b5752d476140196
Biswa Saraf <117895834+Biswa4414@users.noreply.github.com>
2024-04-03 19:56:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const store = new mongoDbSession({
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://educase-client.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
